e531e96e44c13182536f25d551d11ab81103e0e2
Pokita <84108412+Pipodestroyer@users.noreply.github.com>
2026-04-23 00:16:23 -0500

    paging

---
+++ js/game.js
@@ -0,0 +1,27 @@
+const botones = document.getElementById('send');
+const definput = document.getElementById('default-input');
+
+function manage(){
+    if(definput.value.trim()===''){
+        botones.disabled = true;
+    } else {
+        botones.disabled = false;
+    }
+}
+
+botones.addEventListener('click', function() { 
+        document.body.classList.add('fade-out');
+            document.body.classList.remove('fade-in');
+            setTimeout(() =>{
+                localStorage.setItem("username", `${definput.value}`)
+                window.location.href = "game.html";
+        }, 1000);
+    
+});
+
+definput.addEventListener('input', manage);
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  document.body.classList.add('fade-in');
+    botones.disabled = true;
+});
